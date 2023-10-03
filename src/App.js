47a784ade1b4bb51031ad2f2da4a7ccf6c83606e
ch <chlgur13@kakao.com>
2023-10-03 17:21:49 +0900

    using reducer for CREATE and UPDATE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer,useRef } from 'react';
 import { Routes,Route,Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
@@ -7,11 +7,53 @@ import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 
 function reducer(state, action) {
+  console.log(state)
+  switch (action.type){
+    case "CREATE": {
+      return [action.data, ...state];
+    }
+    default: {
+      return state;
+    }
+    case "UPDATE": {
+      return state.map((it) => 
+      String(it.id) === String(action.data.id) ? {...action.data} : it
+      );
+    }
+  }
   return state
 }
 
 function App() {
   const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+  
+  //생성
+  const onCreate = (date, content, emotionId) => {
+    dispatch({
+      type : "CREATE",
+      data : {
+        id:idRef.current,
+        date:new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+    idRef.current += 1;
+  }
+
+  //수정
+  const onUpdate = (targetId,date,content,emotionId) => {
+    dispatch( {
+      type : "UPDATE",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  };
   return(
     <div className="App">
       <Routes>
