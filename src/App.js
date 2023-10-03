1d7f82e2d67bc5e3392286df70d2bb1a2fc5f105
ch <chlgur13@kakao.com>
2023-10-03 16:56:54 +0900

    10-03 second commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useReducer } from 'react';
 import { Routes,Route,Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
@@ -5,9 +6,12 @@ import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 
-
+function reducer(state, action) {
+  return state
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, []);
   return(
     <div className="App">
       <Routes>
@@ -16,12 +20,6 @@ function App() {
         <Route path='/diary/:id' element={<Diary />} />
         <Route path='/edit' element={<Edit />} />
       </Routes>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        <Link to={"/edit"}>Edit</Link>
-      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,10 @@
-import Button from "../component/Button";
-import Header from "../component/Header";
-import Editor from "../component/Editor";
+
+
 
 const Home = () => {
-    return(
-     <div>
-        <Editor 
-        initData={{
-            date: new Date().getTime(),
-            emotionId:1,
-            content:"이전에 작성했던 일기",
-        }}
-        onSubmit={()=>{
-            alert("작성 완료 버튼을 클릭했음");
-        }}
-        />
-     </div>
-    );   
+    return<div>Home 페이지</div>
+    
+     
 };
 export default Home;
 
