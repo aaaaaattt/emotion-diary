3c687a62288f6170260b5332041a08524bb99c80
ch <chlgur13@kakao.com>
2023-10-03 12:10:08 +0900

    emotion, date, diary...

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,8 +1,6 @@
 import "./Button.css"
 
 const Button = ({text, type, onClick }) => {
-    console.log(text)
-    console.log(type)
     const btnType = ["positive", "negative"].includes(type) ? type : "default";
   return(
     <button
